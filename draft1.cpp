12d520796465112e9a2a5180d75edc52cf65a878
angelarodriguez <owner@Owners-MacBook-Air.local>
2024-03-16 16:35:03 -0400

    adding input

--- draft1.cpp
+++ draft1.cpp
@@ -4,5 +4,8 @@ using namespace std;
 // main() is where program execution begins.
 int main() {
    cout<<"please send us your name";
+   string name;
+   cout<<"What is your name?";
+   cin>>name;
    return 0;
 }
