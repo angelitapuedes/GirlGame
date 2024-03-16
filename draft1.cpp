5c872d9d1d1420bd1e6b464332184994f6402eb8
angelarodriguez <owner@Owners-MacBook-Air.local>
2024-03-16 17:01:04 -0400

    draft4

--- draft1.cpp
+++ draft1.cpp
@@ -7,16 +7,23 @@ int main() {
    string name;
    cout<<"What is your name?";
    cin>>name;
-   cout<<"Hi"+name;
-   cout<<"What 4 types of husbands would you want to have financially";
+   cout<<"Hi"+name+ "\n";
+   cout<<"What 4 types of husbands would you want to have financially\n";
    string husband1, husband2, husband3, husband4;
-   cout<<"Husband1";
+   cout<<"Husband1 \n";
    cin>>husband1;
-   cout<<"Husband2";
+   cout<<"\n";
+   cout<<"Husband2 \n";
    cin>>husband2;
-   cout<<"Husband3";
+   cout<<"\n";
+   cout<<"Husband3 \n";
    cin>>husband3;
-   cout<<"Husband4";
+   cout<<"\n";
+   cout<<"Husband4 \n";
    cin>>husband4;
+   cout<<"\n";
+   cout<<"Options\n";
+   cout<<husband1+husband2+husband3+husband4;
+
    return 0;
 }
